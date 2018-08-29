eb5a15239e9e1895b2321944862baea442db1ed0
david.guy-cazalets.external <dguy-caz@student.42.fr>
2018-08-29 22:02:14 +0200

    improved sidebar for mobile

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import cssClasses from './DrawerToggle.css';
 
 const drawerToggle = (props) => (
-  <div onClick={props.clicked}>MENU</div>
+  <div
+    className={cssClasses.DrawerToggle}
+    onClick={props.clicked}>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
 );
 
 export default drawerToggle;
