9a8ad5749aeed6986f70b3cb0af4e80e1ab7d176
david.guy-cazalets.external <dguy-caz@student.42.fr>
2018-08-29 22:23:30 +0200

    improve performance with the OrderSummary

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,19 +1,27 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import cssClasses from './Modal.css';
 import Backdrop from '../Backdrop/Backdrop'
 
-const modal = (props) => (
-  <Fragment>
-    <Backdrop show={props.show} clickedBackdrop={props.modalClosed}/>
-    <div
-      className={cssClasses.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(100vh)',
-        opacity: props.show ? '1' : '0'
-      }}>
-      {props.children}
-    </div>
-  </Fragment>
-);
+class Modal extends Component {
+  shouldComponentUpdate (nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
 
-export default modal;
+  render() {
+    return (
+      <Fragment>
+        <Backdrop show={this.props.show} clickedBackdrop={this.props.modalClosed} />
+        <div
+          className={cssClasses.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}>
+          {this.props.children}
+        </div>
+      </Fragment>
+    )
+  }
+}
+
+export default Modal;
